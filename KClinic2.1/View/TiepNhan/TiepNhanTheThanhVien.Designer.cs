3a414c9cccf9e3c1b8308812797becded98190ee
ThaiLTN <nguyenthai54@gmail.com>
2024-03-26 15:17:53 +0700

    add form thong tin the thanh vien

---
+++ KClinic2.1/View/TiepNhan/TiepNhanTheThanhVien.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace KClinic2._1.View.TiepNhan
+{
+    partial class TiepNhanTheThanhVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TiepNhanTheThanhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 307);
+            this.Name = "TiepNhanTheThanhVien";
+            this.Text = "Tiếp nhận bệnh nhân sử dụng thẻ thành viên";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ KClinic2.1/View/TiepNhan/TiepNhanTheThanhVien.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KClinic2._1.View.TiepNhan
+{
+    public partial class TiepNhanTheThanhVien : DevExpress.XtraEditors.XtraForm
+    {
+        public TiepNhanTheThanhVien()
+        {
+            InitializeComponent();
+        }
+    }
+}
