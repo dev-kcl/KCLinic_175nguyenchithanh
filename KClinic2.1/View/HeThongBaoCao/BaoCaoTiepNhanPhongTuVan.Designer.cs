08a4db50f098096af565b7e4f80bc8c6796d4287
hiennt4776 <87227357+hiennt4776@users.noreply.github.com>
2024-06-17 14:37:03 +0700

    update Bao Cao

---
+++ KClinic2.1/View/HeThongBaoCao/BaoCaoTiepNhanPhongTuVan.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace KClinic2._1.View.HeThongBaoCao
+{
+    partial class BaoCaoTiepNhanPhongTuVan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnXem = new DevExpress.XtraEditors.SimpleButton();
+            this.txtTuNgay = new Janus.Windows.CalendarCombo.CalendarCombo();
+            this.txtDenNgay = new Janus.Windows.CalendarCombo.CalendarCombo();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbbPhongTuVan = new Janus.Windows.EditControls.UIComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMain
+            // 
+            this.panelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMain.Controls.Add(this.cbbPhongTuVan);
+            this.panelMain.Controls.Add(this.label3);
+            this.panelMain.Controls.Add(this.label2);
+            this.panelMain.Controls.Add(this.btnXem);
+            this.panelMain.Controls.Add(this.txtTuNgay);
+            this.panelMain.Controls.Add(this.txtDenNgay);
+            this.panelMain.Controls.Add(this.label1);
+            this.panelMain.Location = new System.Drawing.Point(34, 42);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(498, 312);
+            this.panelMain.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(84, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 19);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Từ ngày";
+            // 
+            // btnXem
+            // 
+            this.btnXem.Location = new System.Drawing.Point(298, 200);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(90, 23);
+            this.btnXem.TabIndex = 16;
+            this.btnXem.Text = "Xem";
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            this.btnXem.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+            // 
+            // txtTuNgay
+            // 
+            this.txtTuNgay.CustomFormat = "dd/MM/yyyy";
+            this.txtTuNgay.DateFormat = Janus.Windows.CalendarCombo.DateFormat.Custom;
+            // 
+            // 
+            // 
+            this.txtTuNgay.DropDownCalendar.FirstMonth = new System.DateTime(2023, 1, 1, 0, 0, 0, 0);
+            this.txtTuNgay.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTuNgay.IsNullDate = true;
+            this.txtTuNgay.Location = new System.Drawing.Point(184, 68);
+            this.txtTuNgay.Name = "txtTuNgay";
+            this.txtTuNgay.Size = new System.Drawing.Size(205, 26);
+            this.txtTuNgay.TabIndex = 13;
+            this.txtTuNgay.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+            // 
+            // txtDenNgay
+            // 
+            this.txtDenNgay.CustomFormat = "dd/MM/yyyy";
+            this.txtDenNgay.DateFormat = Janus.Windows.CalendarCombo.DateFormat.Custom;
+            // 
+            // 
+            // 
+            this.txtDenNgay.DropDownCalendar.FirstMonth = new System.DateTime(2023, 1, 1, 0, 0, 0, 0);
+            this.txtDenNgay.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDenNgay.IsNullDate = true;
+            this.txtDenNgay.Location = new System.Drawing.Point(184, 112);
+            this.txtDenNgay.Name = "txtDenNgay";
+            this.txtDenNgay.Size = new System.Drawing.Size(205, 26);
+            this.txtDenNgay.TabIndex = 15;
+            this.txtDenNgay.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(84, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 19);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Đến ngày";
+            // 
+            // cbbPhongTuVan
+            // 
+            this.cbbPhongTuVan.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbPhongTuVan.Location = new System.Drawing.Point(184, 153);
+            this.cbbPhongTuVan.Name = "cbbPhongTuVan";
+            this.cbbPhongTuVan.Size = new System.Drawing.Size(204, 26);
+            this.cbbPhongTuVan.TabIndex = 70;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(84, 160);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 19);
+            this.label3.TabIndex = 69;
+            this.label3.Text = "Phòng tư vấn";
+            // 
+            // BaoCaoTiepNhanPhongTuVan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 396);
+            this.Controls.Add(this.panelMain);
+            this.Name = "BaoCaoTiepNhanPhongTuVan";
+            this.Text = "Báo cáo doanh thu phòng khám";
+            this.Load += new System.EventHandler(this.BaoCaoBNChiDinhDichVu_Load);
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Label label2;
+        private DevExpress.XtraEditors.SimpleButton btnXem;
+        private Janus.Windows.CalendarCombo.CalendarCombo txtTuNgay;
+        private Janus.Windows.CalendarCombo.CalendarCombo txtDenNgay;
+        private System.Windows.Forms.Label label1;
+        private Janus.Windows.EditControls.UIComboBox cbbPhongTuVan;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ KClinic2.1/View/HeThongBaoCao/BaoCaoTiepNhanPhongTuVan.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace KClinic2._1.View.HeThongBaoCao
+{
+    public partial class BaoCaoTiepNhanPhongTuVan : DevExpress.XtraEditors.XtraForm
+    {
+        public BaoCaoTiepNhanPhongTuVan()
+        {
+            InitializeComponent();
+        }
+
+        private void BaoCaoBNChiDinhDichVu_Load(object sender, EventArgs e)
+        {
+            panelMain.Location = new Point(
+            this.ClientSize.Width / 2 - panelMain.Size.Width / 2,
+            this.ClientSize.Height / 2 - panelMain.Size.Height / 2);
+            panelMain.Anchor = AnchorStyles.None;
+
+
+            DataTable PhongTuVan = Model.DbTiepNhan.CbbPhongTuVan();
+            cbbPhongTuVan.DataSource = PhongTuVan;
+            cbbPhongTuVan.ValueMember = "FieldCode";
+            cbbPhongTuVan.DisplayMember = "FieldName";
+            cbbPhongTuVan.SelectedValue = "4";
+
+            txtTuNgay.Value = DateTime.Now;
+            txtDenNgay.Value = DateTime.Now;
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+
+            string PhongTuVan_ID = "null";
+            if (cbbPhongTuVan.SelectedItem != null) { PhongTuVan_ID = cbbPhongTuVan.SelectedValue.ToString(); }
+            View.HeThongBaoCao.Report.MaBaoCao = "BC019";
+            string TuNgay = "'" + txtTuNgay.Value.ToString("yyyyMMdd") + "'";
+            string DenNgay = "'" + txtDenNgay.Value.ToString("yyyyMMdd") + "'";
+            View.HeThongBaoCao.Report.TableBaoCao = Model.dbBaoCao.SP_BaoCao_019_BaoCaoKhachHangPhongTuVan(TuNgay, DenNgay, PhongTuVan_ID, Login.UserName);
+            View.HeThongBaoCao.Report bc = new View.HeThongBaoCao.Report();
+            bc.Show();
+        }
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Tab && e.Shift)
+            {
+                MoveFocusToPreviousTextbox();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void MoveFocusToPreviousTextbox()
+        {
+            Control currentControl = this.ActiveControl;
+
+            Control[] controls = this.Controls.Cast<Control>().ToArray();
+
+            int currentIndex = Array.IndexOf(controls, currentControl);
+            int previousIndex = (currentIndex - 1 + controls.Length) % controls.Length;
+
+            controls[previousIndex].Focus();
+        }
+    }
+}
